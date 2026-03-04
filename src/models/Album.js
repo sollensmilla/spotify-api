32b876df6b4d68ead46b2ac2dc0818c25970737a
Smilla Sollén <ss226uk@student.lnu.se>
2026-03-04 15:06:06 +0100

    Remove models

--- src/models/Album.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const albumSchema = new mongoose.Schema({
-    album_id: { type: String, required: true, unique: true },
-    album_name: String,
-    total_tracks: Number
-});
-
-export default mongoose.model("Album", albumSchema);
