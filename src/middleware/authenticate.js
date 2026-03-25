dea373ae31dc763a24e802cb0889755bdb01b9a8
Smilla Sollén <ss226uk@student.lnu.se>
2026-03-25 22:24:26 +0100

    Make sure auth does not return decoded token

--- src/middleware/authenticate.js
+++ src/middleware/authenticate.js
@@ -7,11 +7,14 @@ import { verifyToken } from '../utils/jwt.js'
 export const authenticate = (req) => {
   const authHeader = req.headers.authorization
 
-  if (!authHeader) return null
-
-  if (!authHeader.startsWith('Bearer ')) return null
+  if (!authHeader?.startsWith('Bearer ')) return null
 
   const token = authHeader.split(' ')[1]
+  const decoded = verifyToken(token)
 
-  return verifyToken(token)
+  if (!decoded) return null
+
+  return {
+    id: decoded.sub
+  }
 }
