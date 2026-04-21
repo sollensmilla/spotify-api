865bcfb03f95cc9857bfbc0883ddf69a913817f5
Smilla Sollén <ss226uk@student.lnu.se>
2026-04-21 10:07:39 +0200

    Order by random

--- src/services/trackService.js
+++ src/services/trackService.js
@@ -128,7 +128,7 @@ export class TrackService {
 
     const res = await this.pool.query(
       `${baseQuery}
-   ORDER BY popularity DESC
+   ORDER BY RANDOM()
    LIMIT $${values.length - 1} OFFSET $${values.length}`,
       values
     )
